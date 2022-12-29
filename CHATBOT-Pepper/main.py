13496fc1acf44762785b0cf7bf1a66a1fb22f03d
safwan <cruelpennywise@gmail.com>
2022-12-29 21:59:18 +0700

    add/env to move token & try to make cogs

--- CHATBOT-Pepper/main.py
+++ CHATBOT-Pepper/main.py
@@ -7,12 +7,19 @@ from discord.voice_client import VoiceClient
 import youtube_dl
 from random import choice
 from asyncio import sleep as s
+from dotenv import load_dotenv
 
+# ENV
+load_dotenv()
+
+token = os.environ['TOKEN']
+
+#
 intents = discord.Intents.all()
 intents.presences = True
 
 intents.message_content = True
-intents.voice_states = True      
+intents.voice_states = True
 
 youtube_dl.utils.bug_reports_message = lambda: ''
 
@@ -27,7 +34,8 @@ ytdl_format_options = {
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+    # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'source_address': '0.0.0.0'
 }
 
 ffmpeg_options = {
@@ -35,6 +43,7 @@ ffmpeg_options = {
 }
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
 
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.5):
@@ -60,19 +69,23 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
 client = commands.Bot(command_prefix='!p ', intents=intents)
 
-TOKEN = 'MTAyOTQ0NTY5MjIxODk0MTQ2MA.GCA4Xv.rtzvOzBzUtgeekxA8ze2BnBm_g8uxW810E4J5k' 
+TOKEN = 'MTAyOTQ0NTY5MjIxODk0MTQ2MA.GCA4Xv.rtzvOzBzUtgeekxA8ze2BnBm_g8uxW810E4J5k'
 
-status = ['Jamming out to music!', 'Scam', 'DDoS', 'KALI', 'Phising', 'Eating!', 'Sleeping!', 'Hacking!', 'Coding!', 'Maintenance!']
+status = ['Jamming out to music!', 'Scam', 'DDoS', 'KALI', 'Phising',
+          'Eating!', 'Sleeping!', 'Hacking!', 'Coding!', 'Maintenance!']
+
 
 @client.event
 async def on_ready():
     change_status.start()
     print('Bot is online! as Pepper PEW PEW')
 
+
 @client.event
 async def on_member_join(member):
     channel = discord.utils.get(member.guild.channels, name='welcome-channel')
     await channel.reply(f'Welcome {member.mention}!  Ready to jam out? See `!p help` command for details!')
+
 
 @client.event
 async def on_member_remove(member):
@@ -84,35 +97,43 @@ async def on_member_remove(member):
 async def ping(ctx):
     await ctx.send(f'**Pong!** Latency: {round(client.latency * 1000)}ms')
 
+
 @client.command(name='hello', help='This command returns a random welcome message')
 async def hello(ctx):
-    responses = ['***grumble*** Why did you wake me up?', 'Top of the morning to you lad!', 'Hello, how are you?', 'Hi', '**Wasssuup!**']
+    responses = ['***grumble*** Why did you wake me up?',
+                 'Top of the morning to you lad!', 'Hello, how are you?', 'Hi', '**Wasssuup!**']
     await ctx.reply(choice(responses))
+
 
 @client.command(name='die', help='This command returns a random last words')
 async def die(ctx):
-    responses = ['why have you brought my short life to an end', 'i could have done so much more', 'i have a family, kill them instead']
+    responses = ['why have you brought my short life to an end',
+                 'i could have done so much more', 'i have a family, kill them instead']
     await ctx.reply(choice(responses))
 
+
 @client.command(name='credits', help='This command returns the credits')
-async def credits(ctx ,member:discord.Member = None):
+async def credits(ctx, member: discord.Member = None):
     if member == None:
         member = ctx.author
-    
+
     name = member.display_name
     fpf = member.display_avatar
 
-    embed = discord.Embed(title="Pepper Develop by.", description="Mangendre Corp. X NES Extrem", colour=discord.Colour.dark_red())
-    embed.set_author(name="Click About Me.", 
-                     url="https://mgdrxnes.taplink.ws", 
+    embed = discord.Embed(title="Pepper Develop by.",
+                          description="Mangendre Corp. X NES Extrem", colour=discord.Colour.dark_red())
+    embed.set_author(name="Click About Me.",
+                     url="https://mgdrxnes.taplink.ws",
                      icon_url="https://res.cloudinary.com/dfjwuorv1/image/upload/v1670604234/Logo_Tour_Guide_Ilustrasi_Kuning__3_-removebg-preview_lozkhc.png")
     embed.set_footer(text="Thanks for using Pepper Bot!")
 
     await ctx.reply(embed=embed)
 
+
 @client.command(name='creditz', help='This command returns the TRUE credits')
 async def creditz(ctx):
     await ctx.send('**No one but me, lozer!**')
+
 
 @client.command(name='play', help='This command plays music')
 async def play(ctx, url):
@@ -130,68 +151,79 @@ async def play(ctx, url):
 
     async with ctx.typing():
         player = await YTDLSource.from_url(url, loop=client.loop)
-        voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-    
-    embed = discord.Embed(title="Now Playing :", 
+        voice_channel.play(player, after=lambda e: print(
+            'Player error: %s' % e) if e else None)
+
+    embed = discord.Embed(title="Now Playing :",
                           description=" {}".format(player.title), colour=discord.Colour.dark_purple())
-    embed.set_author(name="Just Enjoy", 
+    embed.set_author(name="Just Enjoy",
                      icon_url="https://res.cloudinary.com/dfjwuorv1/image/upload/v1670603009/Logo_Tour_Guide_Ilustrasi_Kuning__2_-removebg-preview_brhhv3.png")
     await ctx.send(embed=embed)
+
 
 @client.command(name='stop', help='This command stops the music and makes the bot leave the voice channel')
 async def stop(ctx):
     voice_client = ctx.message.guild.voice_client
     await voice_client.disconnect()
 
+
 @client.command(name='pause', help='This command stops the music and makes the bot leave the voice channel')
 async def pause(ctx):
     voice_client = ctx.message.guild.voice_client
     await voice_client.pause()
+
 
 @client.command(name='resume', help='This command stops the music and makes the bot leave the voice channel')
 async def resume(ctx):
     voice_client = ctx.message.guild.voice_client
     await voice_client.resume()
 
+
 @client.command(name='info', help='This command give the information to member')
-async def info(ctx ,member:discord.Member = None):
+async def info(ctx, member: discord.Member = None):
     if member == None:
         member = ctx.author
-    
+
     name = member.display_name
     fpf = member.display_avatar
 
-    embed = discord.Embed(title="Welcome Our Server", description="Pepper Ready to Seshhhh with Smile :) ", colour=discord.Colour.random())
-    embed.set_author(name="Hi! Invite Me by Click.", 
-                     url="https://discord.com/api/oauth2/authorize?client_id=1029445692218941460&permissions=0&scope=bot", 
+    embed = discord.Embed(title="Welcome Our Server",
+                          description="Pepper Ready to Seshhhh with Smile :) ", colour=discord.Colour.random())
+    embed.set_author(name="Hi! Invite Me by Click.",
+                     url="https://discord.com/api/oauth2/authorize?client_id=1029445692218941460&permissions=0&scope=bot",
                      icon_url="https://res.cloudinary.com/dfjwuorv1/image/upload/v1670601820/Logo_Tour_Guide_Ilustrasi_Kuning__1_-removebg-preview_nn4fon.png")
     embed.set_thumbnail(url=f"{fpf}")
     embed.add_field(name="Hi! Buddy", value=f"{name}", inline=True)
-    embed.add_field(name="Command Help !", value="If u need help just command `?help`", inline=False)
+    embed.add_field(name="Command Help !",
+                    value="If u need help just command `?help`", inline=False)
     embed.set_footer(text=f"{name} Thanks a bunch !")
 
     await ctx.reply(embed=embed)
-    
+
+
 @client.command(name='button', help='masih dikembagin')
 async def button(ctx):
     # Create the embed message
-    embed = discord.Embed(title='Button', description='Click the button to send a message')
+    embed = discord.Embed(
+        title='Button', description='Click the button to send a message')
 
     # Add the button
-    embed.add_field(name='Button', value='[Click here](https://google.com)', inline=False)
+    embed.add_field(
+        name='Button', value='[Click here](https://google.com)', inline=False)
 
     # Send the message
     await ctx.send(embed=embed)
+
 
 @client.command(name='remind', help='This command give the remind for information')
 async def remind(ctx, time, task):
     def convert(time):
         pos = ['s', 'm', 'h', 'd']
-        
-        time_dict = {"s":1, "m":60, "h":3600, "d":3600*24}
-        
+
+        time_dict = {"s": 1, "m": 60, "h": 3600, "d": 3600*24}
+
         unit = time[-1]
-        
+
         if unit not in pos:
             return -1
         try:
@@ -199,27 +231,25 @@ async def remind(ctx, time, task):
         except:
             return -2
         return val * time_dict[unit]
-    
+
     converted_time = convert(time)
-    
+
     if converted_time == -1:
         await ctx.send("You didn't answer the time correctly.")
         return
-    
+
     if converted_time == -2:
         await ctx.send("The Time mush be an integer")
         return
-    
+
     await ctx.send(f"Started reminder for **{task}** and will last **{time}**.")
-    
-    
+
     await s(converted_time)
     await ctx.reply(f"{ctx.author.mention} your reminder for **{task}** has finished")
-    
-    
-    
+
+
 @tasks.loop(seconds=20)
 async def change_status():
     await client.change_presence(activity=discord.Game(choice(status)))
 
-client.run(TOKEN)
+client.run(token)
