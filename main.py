739434a96d0f6bad4001f7d03e8c48d659531328
BryanFlava <bray.gaul99@gmail.com>
2023-01-02 09:44:34 +0700

    add/menambah max-token

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ async def on_message(message):
         model="text-davinci-003",
         prompt=prompt,
         temperature=0.9,
-        max_tokens=150,
+        max_tokens=1024,
         top_p=1,
         frequency_penalty=0.0,
         presence_penalty=0.6,
